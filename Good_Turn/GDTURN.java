024ce32f513d393db32e594bc548559183261e8f
Ikramul-m <i20727172@gmail.com>
2023-07-01 09:17:20 +0530

    Good Turn 2

--- Good_Turn/GDTURN.java
+++ Good_Turn/GDTURN.java
@@ -1,34 +1,27 @@
 /* package codechef; // don't place package name! */
 
-import java.util.*;
-import java.lang.*;
 import java.io.*;
+import java.lang.*;
+import java.util.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class Codechef
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		Scanner sc = new Scanner(System.in);
-		
-		int t = sc.nextInt();
-		
-		for(int i=1 ; i<=t ; i++){
-		    	int n = sc.nextInt();
-		int m = sc.nextInt();
-		
-		if((n+m) > 6)
-		{
-		    System.out.println("YES");
-		}
-		else
-		{
-		    System.out.println("NO");
-		}
-		}
-		
-	
-		
-// 		System.out.println(n + " " +m);
-	}
+class Codechef {
+
+  public static void main(String[] args) throws java.lang.Exception {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+
+    for (int i = 1; i <= t; i++) {
+      int n = sc.nextInt();
+      int m = sc.nextInt();
+
+      if ((n + m) > 6) {
+        System.out.println("YES");
+      } else {
+        System.out.println("NO");
+      }
+    }
+    // 		System.out.println(n + " " +m);
+  }
 }
