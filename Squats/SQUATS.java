6bb5ac39fa685007e94d3c3e5883bbd9de5af6af
Ikramul-m <i20727172@gmail.com>
2023-07-01 10:30:52 +0530

    Squats

---
+++ Squats/SQUATS.java
@@ -0,0 +1,24 @@
+/* package codechef; // don't place package name! */
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef {
+
+  public static void main(String[] args) throws java.lang.Exception {
+    // your code goes here
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+
+    for (int i = 1; i <= t; i++) {
+      int x = sc.nextInt();
+
+      int s = 15 * x;
+
+      System.out.println(s);
+    }
+  }
+}
