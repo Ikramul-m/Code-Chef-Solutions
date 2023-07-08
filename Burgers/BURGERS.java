b63ab84ade6b1b846dc1669ba423b9e689ddf230
Ikramul-m <i20727172@gmail.com>
2023-07-08 12:11:19 +0530

    Burgers

---
+++ Burgers/BURGERS.java
@@ -0,0 +1,37 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+		
+		for(int i=1 ; i<=t ; i++)
+		{
+		    int a = sc.nextInt();
+		    int b = sc.nextInt();
+		    
+		    if(a==b)
+		    {
+		        System.out.println(a);
+		    }
+		    else
+		    {
+		        if(a>b)
+		        {
+		            System.out.println(b);
+		        }
+		        else{
+		            System.out.println(a);
+		        }
+		    }
+		}
+	}
+}
