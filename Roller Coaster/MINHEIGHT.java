03c33474a958e1fd848ad12a5cb2839607823fe9
Ikramul-m <i20727172@gmail.com>
2023-07-10 11:05:12 +0530

    Roller Coaster

---
+++ Roller Coaster/MINHEIGHT.java
@@ -0,0 +1,31 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+		
+		for(int i=1 ; i<=t ; i++)
+		{
+		    int x = sc.nextInt();//Height of Chef's son
+		    int h = sc.nextInt();//Minimum height required for the ride
+		    
+		    if(h<=x)
+		    {
+		        System.out.println("YES");
+		    }
+		    else
+		    {
+		        System.out.println("NO");
+		    }
+		}
+	}
+}
